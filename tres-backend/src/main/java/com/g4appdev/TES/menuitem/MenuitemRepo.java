73f4fec5f592b238687ff80c398a28e6f5e14fc7
Smuffinn <stevenjantabungar@gmail.com>
2024-11-19 12:43:37 +0800

    chuchu

--- tres-backend/src/main/java/com/g4appdev/TES/menuitem/MenuitemRepo.java
+++ tres-backend/src/main/java/com/g4appdev/TES/menuitem/MenuitemRepo.java
@@ -2,7 +2,7 @@ package com.g4appdev.TES.menuitem;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
+
 
 @Repository
 public interface MenuitemRepo extends JpaRepository<MenuitemEntity, Integer> {
